b22d3749b9578f1c1973d9bdc6079a96e548283a
Animish2002 <animishchopade123@gmail.com>
2025-07-29 14:07:21 +0530

    new changes in server worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,39 @@
 // public/service-worker.js
 
+// Version 1.1 - Added a comment to force update
 // This is the service worker file. It should be placed in the `public` directory
 // and registered in your main React app entry point (e.g., index.js).
 
-self.addEventListener('push', function(event) {
+self.addEventListener("push", function (event) {
   const data = event.data ? event.data.json() : {};
-  const title = data.title || 'FitFocus Notification';
+  const title = data.title || "FitFocus Notification";
   const options = {
-    body: data.body || 'You have a new update from FitFocus!',
-    icon: data.icon || 'https://res.cloudinary.com/dkv3bx51z/image/upload/v1753765682/FitFocus_da7uvi.png', // Default icon
-    badge: data.badge || 'https://res.cloudinary.com/dkv3bx51z/image/upload/v1753765772/bell_l81cml.png', // Small icon for notification tray on some OS
+    body: data.body || "You have a new update from FitFocus!",
+    icon:
+      data.icon ||
+      "https://res.cloudinary.com/dkv3bx51z/image/upload/v1753765682/FitFocus_da7uvi.png", // Default icon
+    badge:
+      data.badge ||
+      "https://res.cloudinary.com/dkv3bx51z/image/upload/v1753765772/bell_l81cml.png", // Small icon for notification tray on some OS
     data: {
-      url: data.url || '/', // URL to open when notification is clicked
+      url: data.url || "/", // URL to open when notification is clicked
     },
     vibrate: [200, 100, 200], // Vibration pattern
-    actions: [ // Optional actions
-      { action: 'open_url', title: 'Go to App' },
+    actions: [
+      // Optional actions
+      { action: "open_url", title: "Go to App" },
       // { action: 'mark_as_read', title: 'Mark as Read' }, // Example
-    ]
+    ],
   };
 
-  event.waitUntil(
-    self.registration.showNotification(title, options)
-  );
+  event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', function(event) {
+self.addEventListener("notificationclick", function (event) {
   event.notification.close(); // Close the notification
 
   const clickedNotification = event.notification.data;
-  const urlToOpen = clickedNotification.url || '/';
+  const urlToOpen = clickedNotification.url || "/";
 
   event.waitUntil(
     clients.openWindow(urlToOpen) // Open the URL when clicked
